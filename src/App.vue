368643384122549b6289d19395dabdda33c3071d
sppooja-iotred <sppooja@iotred.com>
2021-01-15 13:26:47 +0100

    sports-app

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <div class="nav-menu">
+      <router-link to="/">Members</router-link>  | 
+      <router-link to="/about">Sports</router-link>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 25px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #7ECEF6;
+}
+
+.nav-menu{
+  float: right;
+}
+</style>
---
+++ src/components/MembersItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="members-item">
+    <div class="gender">
+      <div v-if= "member.gender === 'male'"> Male </div>
+      <div v-else> Female </div>
+    </div>
+    <div class="name">
+      <span class="first">
+        {{ member.firstName }}
+      </span>
+      <span class="last">
+        {{ member.lastName | capitalize}}
+      </span>
+    </div>
+    <div class="birth">
+      {{ dateFormate(member.birthDate) }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'MembersItem',
+
+  props: {
+    member: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+  },
+
+  methods: {
+    dateFormate(birthDate) {
+      return birthDate.toString().split("-").reverse().join("-");
+    }
+  },
+  filters: {
+    capitalize(value) {
+      return value.toUpperCase();
+    }
+  }
+
+};
+</script>
+
+<style>
+.members-item {
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 10px;
+  background: lightblue;
+  padding: 20px;
+}
+
+.last {
+  font-weight: bold;
+}
+
+</style>
